9b0ab73e3a307a88773bd9d42dbb98879728a6f7
DoodMac <48062579+jbird84@users.noreply.github.com>
2024-06-24 21:19:37 -0500

    finished filtering by alphabetical

--- JP Apex Predators/ContentView.swift
+++ JP Apex Predators/ContentView.swift
@@ -11,9 +11,11 @@ struct ContentView: View {
     
     let predators = Predators()
     @State var searchText = ""
+    @State var alphabetical = false
     
     var filteredDinos: [ApexPredator] {
-        predators.search(for: searchText)
+        predators.sort(by: alphabetical)
+        return predators.search(for: searchText)
     }
     
     var body: some View {
@@ -50,6 +52,18 @@ struct ContentView: View {
             .searchable(text: $searchText)
             .autocorrectionDisabled()
             .animation(.default, value: searchText)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        withAnimation {
+                            alphabetical.toggle()
+                        }
+                    } label: {
+                       Image(systemName: "textformat")
+                            .symbolEffect(.bounce, value: alphabetical)
+                    }
+                }
+            }
         }
         .preferredColorScheme(.dark)
     }
