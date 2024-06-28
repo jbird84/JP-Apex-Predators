f71c7722acb168da204216f861cfac3e8c031372
DoodMac <48062579+jbird84@users.noreply.github.com>
2024-06-28 07:50:41 -0500

    continue working on Details view

--- JP Apex Predators/PredatorDetail.swift
+++ JP Apex Predators/PredatorDetail.swift
@@ -14,25 +14,56 @@ struct PredatorDetail: View {
     
     var body: some View {
         GeometryReader { geo in
-        ScrollView {
-            ZStack(alignment: .bottomTrailing) {
-                Image(predator.type.rawValue)
-                    .resizable()
-                    .scaledToFit()
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: geo.size.width/1.5, height: geo.size.height/3)
-                    .scaleEffect(x: -1)
-                    .shadow(color: .black, radius: 7)
-                    .offset(y: 20)
+            ScrollView {
+                ZStack(alignment: .bottomTrailing) {
+                    Image(predator.type.rawValue)
+                        .resizable()
+                        .scaledToFit()
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width/1.5, height: geo.size.height/3)
+                        .scaleEffect(x: -1)
+                        .shadow(color: .black, radius: 7)
+                        .offset(y: 20)
+                }
+                VStack(alignment: .leading) {
+                    //Dino Name
+                    Text(predator.name)
+                        .font(.largeTitle)
+                    //Current Location
+                    
+                    //Apprears in
+                    Text("Appears In:")
+                        .font(.title3)
+                    
+                    ForEach(predator.movies, id: \.self) { movie in
+                        Text("•" + movie)
+                            .font(.subheadline)
+                    }
+                    
+                    //Movie moments
+                    Text("Movie Moments")
+                        .font(.title)
+                        .padding(.top, 15)
+                    
+                    ForEach(predator.movieScenes) { scene in
+                        Text(scene.movie)
+                            .font(.title2)
+                            .padding(.vertical, 1)
+                        Text(scene.sceneDescription)
+                            .padding(.bottom, 15)
+                    }
+                    
+                    //Link to web page
+                Text("Read More:")
+                        .font(.caption)
+                    
+                    Link(predator.link, destination: URL(string: predator.link)!)
+                }
+                .padding()
+                .frame(width: geo.size.width, alignment: .leading)
             }
-            //Dino Name
-            //Current Location
-            //Apprears in
-            //Movie moments
-            //Link to web page
-        }
         .ignoresSafeArea()
     }
     }
